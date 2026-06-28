0e9f9296b0eb7a213bf9ff22e7c3539785f03b3c
Pratishtha <rinibhadoria@gmail.com>
2026-06-28 11:10:30 +0530

    Add day_2/variables.py

---
+++ day_2/variables.py
@@ -0,0 +1,66 @@
+# Day 2: 30 DAys of python programming 
+# Exercise 1
+first_name = 'Rini'
+last_name = 'Bhadoria'
+full_name = 'Rini Bhadoria'
+country = 'India'
+city = 'Jaipur'
+age = '12'
+year = '2026'
+is_married = False
+is_true = True
+is_light_on = True
+
+print(first_name,last_name,country,city,year,age)
+
+# Exercise 2
+
+print (type(is_married))
+print (len(first_name))
+
+if((len(first_name)) == (len(last_name))):
+   print('Equal')
+else:
+   print('Not equal')   
+
+
+num_one = 5 
+num_two = 4
+
+total = num_one + num_two
+print(total)
+
+diff = num_one - num_two
+print(diff)
+
+product = num_one * num_two
+print(product)
+
+division = num_one/num_two
+print(division)
+   
+remainder =num_two%num_one
+print(remainder)
+
+exp = num_one**num_two
+print(exp)
+
+floor_division = num_one//num_two
+print(floor_division)
+
+r = 30
+area_of_circle = 3.14*r*r
+circum_of_circle = 2*3.14*r
+
+print(area_of_circle,circum_of_circle)
+
+radius = int(input('Radius for the new circle: '))
+area = (3.14*radius*radius)
+print(area)
+
+First_name = input('Enter your first name: ')
+Last_name = input('Enter your last name: ')
+Country = input('Enter the name of your country: ')
+Age = int(input('Enter you age: '))
+
+
